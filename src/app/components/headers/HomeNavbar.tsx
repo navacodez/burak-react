a85368ef01804be3799c81e0c65b5b2b47ea2392
navacodez <navacodez@gmail.com>
2025-04-20 22:09:14 +0900

    fix: homeNavbar menu part refactoring

--- src/app/components/headers/HomeNavbar.tsx
+++ src/app/components/headers/HomeNavbar.tsx
@@ -3,28 +3,16 @@ import { NavLink } from "react-router-dom";
 
 export function HomeNavbar() {
     const authMember = null;
-    return <div className="home-navbar">
-      <Container sx={{ mt: "55px", height: "642px" }}>
-        <Stack
-          sx={{ height: "50px" }}
-          flexDirection={"row"}
-          justifyContent={"space-between"}
-          alignItems={"center"}
-        >
+    return (
+      <div className="home-navbar">
+        <Container className="navbar-container">
+          <Stack  className="menu">
           <Box>
             <NavLink to="/">
-              <img
-               style={{ width: "125px", height: "30px" }}
-               src="/icons/burak.svg"
-              />
+            <img className="brand-logo" src="/icons/burak.svg "/>
             </NavLink>
           </Box>
-          <Stack 
-            flexDirection={"row"}
-            justifyContent={"space-between"}
-            minWidth={"700px"}
-            alignItems={"center"}
-          >
+          <Stack className="links">
             <Box className={"hover-line"}>
               <NavLink to="/" activeClassName={"underline"}>
                 Home
@@ -58,18 +46,21 @@ export function HomeNavbar() {
   
             {!authMember ? (
                 <Box>
-                  <Button
-                    variant="contained" style={{ background: "#3776CC", color: "f8f8ff"}}
-                  >
+                   <Button variant="contained" className="login-button">
                       Login
                     </Button>
                 </Box>
               ) : (
-                <img />
+                <img
+                 className="user-avatar"
+                 src={"/icons/default-user.svg"}
+                 aria-haspopup={"true"}
+               />
               )}
           </Stack>
         </Stack>
         <Stack>DETAIL</Stack>
-      </Container>
-    </div>;
+       </Container>
+     </div>
+   );
   }
