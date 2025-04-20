aac982cc52c9ffd8898db8afb841204970ec15a3
navacodez <navacodez@gmail.com>
2025-04-20 21:48:59 +0900

    fix: organize header and footer components

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,47 +2,37 @@ import React from "react";
 import "../css/app.css";
 import { Box, Button, Container, Stack, Typography } from "@mui/material";
 import { RippleBadge } from "./MaterialTheme/styled";
-import { Link, Route, Switch } from "react-router-dom";
+import { Link, Route, Switch,  useLocation} from "react-router-dom";
 import { HomePage } from "./screens/homePage";
 import { ProductsPage } from "./screens/productsPage";
 import { OrdersPage } from "./screens/ordersPage";
 import { UserPage } from "./screens/userPage";
+import { HomeNavbar } from "./components/headers/HomeNavbar";
+import { OtherNavbar } from "./components/headers/OtherNavbar";
+import { Footer } from "./components/footer";
 
 function App() {
+  const location = useLocation();
+  console.log("location:", location);
   return (  
-  <div>
-    <nav>
-      <ul>
-      <li>
-           <Link to="/">HomePage</Link>
-         </li>
-        <li>
-          <Link to="/products">ProductsPag</Link>
-        </li>
-        <li>
-          <Link to="/member-page">UserPage</Link>
-        </li>
-        <li>
-          <Link to="/users">Users</Link>
-        </li>
-      </ul>
-    </nav>
-    
+    <>
+    {location.pathname === "/" ? <HomeNavbar /> : <OtherNavbar />}
     <Switch>
-    <Route path="/products">
-         <ProductsPage />
-       </Route>
-       <Route path="/order">
-         <OrdersPage />
+      <Route path="/products">
+        < ProductsPage />
+      </Route>
+      <Route path="/order">
+        <OrdersPage /> 
       </Route>
       <Route path="/member-page">
-      <UserPage />
+        <UserPage />
       </Route>
       <Route path="/">
-        <HomePage/>
+        <HomePage />
       </Route>
     </Switch>
-  </div>
+    <Footer />
+  </>
   );
 }
 
