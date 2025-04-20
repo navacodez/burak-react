227ab9dcfef2a28bd34aa69660a29da219918ce1
navacodez <navacodez@gmail.com>
2025-04-20 23:44:55 +0900

    fix: modify statistics sectional component

--- src/app/screens/homePage/index.tsx
+++ src/app/screens/homePage/index.tsx
@@ -1,5 +1,22 @@
+import React from "react";
 import { Container } from "@mui/material";
+import Statistics from "./Statistics";
+import PopularDishes from "./PopularDishes";
+import NewDishes from "./NewDishes";
+import Advertisement from "./Advertisement";
+import ActiveUsers from "./ActiveUsers";
+import Events from "./Events";
+import "../../../css/home.css";
  
- export default function HomePage() {
-   return <Container>HomePage</Container>;
- }
+export default function HomePage() {
+  return(
+    <div className={"homepage"}>
+      <Statistics />
+      <PopularDishes />
+      <NewDishes />
+      <Advertisement />
+      <ActiveUsers />
+      <Events />
+    </div>
+  );
+}
