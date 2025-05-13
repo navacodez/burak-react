cd1d562131369aab8eb88fc90d4c3f4d549b3427
navacodez <navacodez@gmail.com>
2025-05-13 23:27:02 +0900

    feat: create homePage Redux configuration

--- src/app/screens/homePage/index.tsx
+++ src/app/screens/homePage/index.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Container } from "@mui/material";
 import Statistics from "./Statistics";
 import PopularDishes from "./PopularDishes";
@@ -9,6 +9,12 @@ import Events from "./Events";
 import "../../../css/home.css";
  
 export default function HomePage() {
+  // Selector: Store => Data
+
+  useEffect(() => {
+   // Backend server data request => Data
+   // Slice: Data => Store
+  }, []);
   return(
     <div className={"homepage"}>
       <Statistics />
