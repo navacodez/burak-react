2343664a21a4292167df06454efa1149d6002f30
navacodez <navacodez@gmail.com>
2025-05-19 20:09:48 +0900

    fix: retrieve freshMenu and modify newDishes component

--- src/app/screens/homePage/index.tsx
+++ src/app/screens/homePage/index.tsx
@@ -6,23 +6,24 @@ import NewDishes from "./NewDishes";
 import Advertisement from "./Advertisement";
 import ActiveUsers from "./ActiveUsers";
 import Events from "./Events";
-import "../../../css/home.css";
 import { useDispatch } from "react-redux";
 import { Dispatch } from "@reduxjs/toolkit";
-import { setPopularDishes } from "./slice";
+import { setNewDishes,  setPopularDishes } from "./slice";
 import { Product } from "./../../../lib/types/product";
 import ProductService from "./../../services/ProductService"
 import { ProductCollection } from "./../../../lib/enums/product.enum";
+import "./../../../css/home.css";
 
 /** REDUX SLICE & SELECTOR **/
 const actionDispatch = (dispatch: Dispatch) => ({
   setPopularDishes: (data: Product[]) => dispatch(setPopularDishes(data)),
+   setNewDishes: (data: Product[]) => dispatch(setNewDishes(data)), 
 });
 
 
 
   export default function HomePage() {
-  const {setPopularDishes} = actionDispatch(useDispatch());
+  const {setPopularDishes, setNewDishes} = actionDispatch(useDispatch());
 
   useEffect(() => {
     // Backend server data fetch => Data 
@@ -32,11 +33,20 @@ const actionDispatch = (dispatch: Dispatch) => ({
       limit: 4,
       order: "productViews",
       productCollection: ProductCollection.DISH,
-    }).then(data => {
+    }).then((data) => {
       setPopularDishes(data)
     })
     .catch(err => console.log(err));
-
+    
+     product.getProducts({
+      page: 1,
+      limit: 4,
+      order: "createdAt",
+      productCollection: ProductCollection.DISH,
+    }).then((data) => {
+      setNewDishes(data);
+    })
+    .catch(err => console.log(err));
   }, []);
 
 
--- src/app/screens/homePage/slice.ts
+++ src/app/screens/homePage/slice.ts
@@ -15,10 +15,10 @@ const homePageSlice = createSlice({
          state.popularDishes = action.payload;
         },
         setNewDishes: (state, action) => {
-            state.popularDishes = action.payload;
+            state.newDishes = action.payload;
            },
            setTopUsers: (state, action) => {
-            state.popularDishes = action.payload;
+            state.topUsers = action.payload;
            },
     },
 });
