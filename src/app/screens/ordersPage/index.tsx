1b04eb1c988dda91cf16822e700a2a4818278331
navacodez <navacodez@gmail.com>
2025-05-30 20:07:02 +0900

    feat: develop updateMember business logic and modify ordersPage component

--- src/app/screens/ordersPage/index.tsx
+++ src/app/screens/ordersPage/index.tsx
@@ -15,6 +15,9 @@ import { Order, OrderInquiry } from "./../../../lib/types/order";
 import { OrderStatus } from "./../../../lib/enums/order.enum";
 import OrderService from "../../services/OrderService";
 import { useGlobals } from "./../../hooks/useGlobals";
+import { useHistory } from "react-router-dom";
+import { serverApi } from "./../../../lib/config";
+import { MemberType } from "./../../../lib/enums/member.enum";
 
 /** REDUX SLICE & SELECTOR **/
 const actionDispatch = (dispatch: Dispatch) => ({
@@ -25,7 +28,8 @@ const actionDispatch = (dispatch: Dispatch) => ({
  
  export default function OrdersPage() {
   const {setPausedOrders, setProcessOrders,  setFinishedOrders} = actionDispatch(useDispatch());
-  const { orderBuilder } = useGlobals();
+  const { orderBuilder, authMember } = useGlobals();
+  const history = useHistory();
   const [value, setValue] = useState("1");
   const [orderInquiry, setOrderInquiry] = useState<OrderInquiry>({
     page: 1,
@@ -59,6 +63,7 @@ const actionDispatch = (dispatch: Dispatch) => ({
   const handleChange = (e: SyntheticEvent, newValue: string) => {
     setValue(newValue);
   };
+  if (!authMember) history.push("/");
   return (
     <div className="order-page">
       <Container className="order-container">
@@ -91,28 +96,29 @@ const actionDispatch = (dispatch: Dispatch) => ({
             <Box className={"member-box"}>
               <div className={"order-user-img"}>
                 <img
-                  src={"/icons/default-user.svg"}
+                  src={authMember?.memberImage ? `${serverApi}/${authMember.memberImage}`
+                    :"/icons/default-user.svg"}
                   className={"order-user-avatar"}
                   alt=""
                 />
 
                 <div className={"order-user-icon-box"}>
                   <img
-                    src={"/icons/user-badge.svg"}
+                    src={authMember?.memberType === MemberType.RESTAURANT ? "/icons/restaurant.svg" : "/icons/user-badge.svg"}
                     className={"order-user-prof-img"}
                     alt=""
                   />
                 </div>
               </div>
-              <span className={"order-user-name"}>Martin</span>
-              <span className={"order-user-prof"}>User</span>
+              <span className={"order-user-name"}>{authMember?.memberNick}</span>Add commentMore actions
+              <span className={"order-user-prof"}>{authMember?.memberType}</span>
             </Box>
             <Box className={"liner"}></Box>
             <Box className={"order-user-address"}>
               <div style={{ display: "flex" }}>
                 <LocationOnIcon />
               </div>
-              <div className={"spec-address-txt"}>Seoul, Korea</div>
+              <div className={"spec-address-txt"}>{authMember?.memberAddress ? authMember.memberAddress : "Do not exist"}</div>
             </Box>
           </Box>
           <Box className={"order-info-box"} sx={{ mt: "15px" }}>
